c67f734fa8f631743d7ff972652a15d62e545f1d
jibo <jibo@outlook.com>
2018-11-23 17:53:02 +0800

    Remove product test data

--- src/app/test/Feed.php
+++ src/app/test/Feed.php
@@ -2,8 +2,6 @@
 
 namespace app\test;
 
-use service\ProductRawData;
-
 class Feed
 {
     const CAROUSEL = [
@@ -18,112 +16,8 @@ class Feed
         ],
     ];
 
-    const SAMPLE = [
-        [
-            'thumb' => 'https://img.alicdn.com/tfscom/i2/1776843727/O1CN011dP0b4298OkOnaU_!!1776843727.jpg_640x0q85s150_.webp',
-            'title' => '大码女装2018初秋新款胖妹妹最爱洋气网红套装减龄毛衣秋季两件套',
-            'coupon' => '券123元',
-            'price' => '122.00',
-            'reservePrice' => '228.00',
-            'url' => 'https://h5.m.taobao.com/awp/core/detail.htm?id=577880881721',
-        ],
-        [
-            'thumb' => 'https://img.alicdn.com/tfscom/i3/2057652301/O1CN011SrtoPubXU2UzdV_!!0-item_pic.jpg_640x0q85s150_.webp',
-            'title' => '冬季外套女2018新款韩版流行毛呢大衣中长款秋冬女装修身呢子外套',
-            'coupon' => '券123元',
-            'price' => '218.00',
-            'reservePrice' => '436.00',
-            'url' => 'https://h5.m.taobao.com/awp/core/detail.htm?id=561566817370',
-        ],
-        [
-            'thumb' => 'https://img.alicdn.com/tfscom/i1/14629272/O1CN012IMcmoXuTuyId1d_!!14629272.jpg_640x0q85s150_.webp',
-            'title' => 'YUYEE_打底开衫两穿坑条长袖针织连衣裙女装秋2018新款长款毛衣裙',
-            'coupon' => '券123元',
-            'price' => '209.00',
-            'reservePrice' => '219.00',
-            'url' => 'https://h5.m.taobao.com/awp/core/detail.htm?id=578796820711',
-        ],
-        [
-            'thumb' => 'https://img.alicdn.com/tfscom/i4/76658125/TB2nlpJbAvoK1RjSZPfXXXPKFXa_!!76658125.jpg_640x0q85s150_.webp',
-            'title' => '法式少女灯芯绒衬衫连衣裙冷系女装维多利亚复古港味气质长裙秋冬',
-            'coupon' => '券123元',
-            'price' => '168.00',
-            'reservePrice' => '168.00',
-            'url' => 'https://h5.m.taobao.com/awp/core/detail.htm?id=577499398296',
-        ],
-        [
-            'thumb' => 'https://img.alicdn.com/tfscom/i1/4077026492/O1CN011xpNjELZ7CewZZ9_!!0-item_pic.jpg_640x0q85s150_.webp',
-            'title' => '加绒打底衫女秋季2018新款秋冬女装修身小衫蕾丝长袖冬季针织上衣',
-            'coupon' => '券123元',
-            'price' => '128.00',
-            'reservePrice' => '228.00',
-            'url' => 'https://h5.m.taobao.com/awp/core/detail.htm?id=575558204873',
-        ],
-        [
-            'thumb' => 'https://img.alicdn.com/tfscom/i3/14629272/O1CN012IMcmQlKrbVfxVK_!!14629272.jpg_640x0q85s150_.webp',
-            'title' => 'YUYEEchic早秋上衣大波点羊毛v领针织衫女装长袖网红毛衣开衫外套',
-            'coupon' => '券123元',
-            'price' => '179.00',
-            'reservePrice' => '179.00',
-            'url' => 'https://h5.m.taobao.com/awp/core/detail.htm?id=577222241952',
-        ],
-        [
-            'thumb' => 'https://img.alicdn.com/tfscom/i4/193502143/O1CN011RhXEcRrkuLUtRD_!!193502143.jpg_640x0q85s150_.webp',
-            'title' => '2018冬装新款东大门羽绒棉服女装中长款过膝棉衣加厚棉袄冬季外套',
-            'coupon' => '券123元',
-            'price' => '299.99',
-            'reservePrice' => '374.99',
-            'url' => 'https://h5.m.taobao.com/awp/core/detail.htm?id=579308010086',
-        ],
-        [
-            'thumb' => 'https://img.alicdn.com/tfscom/i1/76658125/O1CN0129tIUhwGB5keg1u_!!76658125.jpg_640x0q85s150_.webp',
-            'title' => '冷系女装桔梗裙复古港味裙灯芯绒连衣裙女神高冷范气质长裙女秋冬',
-            'coupon' => '券123元',
-            'price' => '138.00',
-            'reservePrice' => '158.00',
-            'url' => 'https://h5.m.taobao.com/awp/core/detail.htm?id=577441029681',
-        ],
-        [
-            'thumb' => 'https://img.alicdn.com/tfscom/i4/193502143/O1CN011RhXEHsUfEvz24M_!!193502143.jpg_640x0q85s150_.webp',
-            'title' => '2018冬季新款水貂绒毛呢外套女装中长款赫本风小个子呢子大衣流行',
-            'coupon' => '券123元',
-            'price' => '479.50',
-            'reservePrice' => '599.38',
-            'url' => 'https://h5.m.taobao.com/awp/core/detail.htm?id=577807027977',
-        ],
-        [
-            'thumb' => 'https://img.alicdn.com/tfscom/i1/720853322/O1CN011aPWBc17dwRgE94_!!720853322.jpg_640x0q85s150_.webp',
-            'title' => '安娜毛衣2018秋季新款百搭宽松针织衫翻领羊毛开衫中长款外套女装',
-            'coupon' => '券123元',
-            'price' => '159.99',
-            'reservePrice' => '279.99',
-            'url' => 'https://h5.m.taobao.com/awp/core/detail.htm?id=576260478455',
-        ],
-    ];
-
     function carousel()
     {
         echo json_encode(self::CAROUSEL, JSON_UNESCAPED_UNICODE);
-    }
-
-    function sample()
-    {
-        echo json_encode(self::SAMPLE, JSON_UNESCAPED_UNICODE);
-    }
-
-    function pagination($f3, $args)
-    {
-        $pageNo = intval($args['pageNo']) ?? 0;
-        $pageSize = 10;
-        $raw = json_decode(file_get_contents(ProductRawData::RAW));
-        $pageTotal = ceil(count($raw) / 10);
-        if ($pageNo >= 1 && $pageNo <= $pageTotal) {
-            $chunk = array_chunk($raw, $pageSize);
-            $data = $chunk[$pageNo - 1];
-        } else {
-            $f3->LOGGER->write("request page $pageNo: no data");
-            $data = [];
-        }
-        echo json_encode($data, JSON_UNESCAPED_UNICODE);
     }
 }
