e4784514a5dac8f7337d7b86543b34ef2f27cdbf
jibo <jibo@outlook.com>
2018-12-01 23:16:10 +0800

    Facebook js-sdk login demo: https://developers.facebook.com/docs/facebook-login/web

---
+++ sql/v0.1.0.sql
@@ -0,0 +1,24 @@
+drop table if exists user;
+create table user (
+  id int unsigned not null auto_increment,
+  affiliate char(6) not null comment 'one\'s affiliate code',
+  referral char(6) not null comment 'referral\'s affiliate code',
+  email varchar(50),
+  mobile varchar(50),
+  status tinyint(1) not null default 0 comment '0:disabled,1:enabled',
+  create_time timestamp not null default current_timestamp,
+  update_time timestamp,
+  facebook json,
+  fid varchar(50) generated always as (facebook ->> '$.id'),
+  primary key (id),
+  index idx_referral (referral),
+  unique idx_affiliate (affiliate),
+  unique idx_user_email (email),
+  unique idx_user_mobile (mobile),
+  unique idx_user_facebook (fid)
+);
+
+insert into user (id,affiliate,referral,status,facebook) values (1,'888888','888888',1,'{"name": "Vivian Lim","id": "123805028625200"}');
+select * from user where facebook->'$.id'='123805028625200';
+select facebook->'$.id',facebook->'$.name' from user;
+select fid from user;
