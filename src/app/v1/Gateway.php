c298d795bba78729b4ad689d189044cc6be39e0c
jibo <jibo@outlook.com>
2018-12-04 11:08:24 +0800

    Refactor Gateway.php

--- src/app/v1/Gateway.php
+++ src/app/v1/Gateway.php
@@ -9,68 +9,51 @@ class Gateway
 {
     const TOKEN_CACHE_TIME = 300;
     private $logger;
-    private $user;
 
-    function product($f3)
+    function product(\Base $f3)
     {
         $id = $f3->get('PARAMS.id');
+        $token = $f3->get('REQUEST.token');
         $detect = new \Mobile_Detect;
         if ($detect->is('WeChat')) {
             $f3->reroute("/v1/wechat/simple/$id");
-        } else if ($this->auth($f3)) {
+        } else if (isset($token) && !is_null($f3->get($token))) {
             header("location:" . $f3->get('GET.url'));
         } else {
             echo \Template::instance()->render('v1/gateway.html');
         }
     }
 
-    function auth($f3)
+    function queryUser(\Base $f3)
     {
-        $token = $_REQUEST['token'] ?? false;
-        $this->logger->write("user token: $token");
-        if ($token !== false) {
-            $this->user = $f3->get($token);
-            if (!is_null($this->user)) {
-                ob_start();
-                var_dump($this->user);
-                $this->logger->write(ob_get_clean());
-                return true;
-            }
-        }
-        $this->logger->write("user auth failed");
-        return false;
-    }
-
-    function queryUser($f3)
-    {
-        $fid = $f3->POST['id'];
+        $fid = $f3->get('POST.id');
         $user = new SqlMapper('user');
         $user->load(['fid=?', $fid]);
         if (!$user->dry()) {
-            $token = $this->setToken($f3, $user);
+            $token = $this->cacheToken($f3, $user);
             echo json_encode($f3->get($token), JSON_UNESCAPED_UNICODE);
         } else {
             echo 'failed';
         }
     }
 
-    function createUser($f3)
+    function createUser(\Base $f3)
     {
-        $fid = $f3->POST['id'];
-        $nickname = $f3->POST['name'];
-        $referral = $f3->POST['referral'];
-        unset($f3->POST['referral']);
+        $fid = $f3->get('POST.id');
+        $nickname = $f3->get('POST.name');
+        $referral = $f3->get('POST.referral');
+        $f3->clear('POST.referral');
         $user = new SqlMapper('user');
         $user->load(['fid=?', $fid]);
         if ($user->dry()) {
             $user['affiliate'] = StringUtils::generateAffiliate();
             $user['referral'] = $referral;
             $user['nickname'] = $nickname;
-            $user['facebook'] = json_encode($f3->POST, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+            $user['facebook'] = json_encode($f3->get('POST'), JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
             $user['update_time'] = date('Y-m-d H:i:s');
             $user->save();
             // TODO: trigger user plan check
-            $token = $this->setToken($f3, $user);
+            $token = $this->cacheToken($f3, $user);
             echo json_encode($f3->get($token), JSON_UNESCAPED_UNICODE);
         } else {
             $this->logger->write("user (fid: $fid) already existed");
@@ -78,9 +61,9 @@ class Gateway
         }
     }
 
-    function validateReferral($f3)
+    function validateReferral(\Base $f3)
     {
-        $referral = $f3->POST['referral'];
+        $referral = $f3->get('POST.referral');
         $user = new SqlMapper('user');
         $user->load(['affiliate=?', $referral]);
         if ($user->dry()) {
@@ -90,15 +73,15 @@ class Gateway
         }
     }
 
-    function setToken(\Base $f3, SqlMapper $user)
+    function cacheToken(\Base $f3, SqlMapper $user)
     {
         $token = StringUtils::generateToken();
         $f3->set($token, array_merge(['token' => $token], $user->cast()), self::TOKEN_CACHE_TIME);
         return $token;
     }
 
-    function beforeRoute($f3)
+    function beforeRoute(\Base $f3)
     {
-        $this->logger = $f3->LOGGER;
+        $this->logger = $f3->get('LOGGER');
     }
 }
