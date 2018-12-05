4604b577a25e3e7810874bb936363a53515fe788
jibo <jibo@outlook.com>
2018-12-05 15:06:55 +0800

    Clearify the allocation of each commission

--- sql/v0.1.0.sql
+++ sql/v0.1.0.sql
@@ -3,6 +3,8 @@ create table user (
   id int unsigned not null auto_increment,
   affiliate char(6) not null comment 'one\'s affiliate code',
   referral char(6) not null comment 'referral\'s affiliate code',
+  plan tinyint(1) not null default 0 comment '会员等级',
+  nickname varchar(50) not null default '',
   email varchar(50),
   mobile varchar(50),
   status tinyint(1) not null default 1 comment '0:disabled,1:enabled',
@@ -18,7 +20,7 @@ create table user (
   unique idx_user_facebook (fid)
 );
 
-insert into user (id,affiliate,referral,status,facebook,update_time) values (1,'888888','888888',1,'{"name": "Vivian Lim","id": "123805028625200"}',now());
+insert into user (id,affiliate,referral,plan,nickname,status,facebook,update_time) values (1,'888888','888888',0,'ezd',1,'{"name": "Vivian Lim","id": "123805028625200"}',now());
 select * from user where facebook->'$.id'='123805028625200';
 select facebook->'$.id',facebook->'$.name' from user;
 select fid from user;
--- sql/v0.1.1.sql
+++ sql/v0.1.1.sql
@@ -1,30 +1,67 @@
 drop table if exists user_plan;
 create table user_plan (
   id int unsigned not null auto_increment,
-  level tinyint(1) not null default 0 comment '0:普通会员,1:付费会员,2:梦想合伙人,3:超级合伙人',
+  level tinyint(1) not null,
+  name varchar(50) not null,
+  description varchar(100) not null,
   primary key (id)
-) comment '梦想合伙人:成功邀请五位付费会员,超级合伙人:邀请的付费会员中产生了五位梦想合伙人';
+);
+insert into user_plan (level,name,description) values (0,'member','普通会员'),(1,'vip','付费会员'),(2,'partner','梦想合伙人'),(3,'super','超级合伙人');
 
-insert into user_plan (id,level) values (0,0),(1,1),(2,2),(3,3);
-
-alter table user add column nickname varchar(50) default '', add column plan tinyint(1) not null default 0;
-update user set nickname='ezd', plan=0 where id=0;
+################################################################################
+# 普通会员付费触发事件
+################################################################################
+# 本人升级为付费会员
+# 检查邀请人升级梦想合伙人条件：付费会员>=100
+# 如果邀请人升级为梦想合伙人，检查邀请人的邀请人升级为超级合伙人条件：梦想合伙人>=10
+################################################################################
 
 drop table if exists quota;
 create table quota (
   id int unsigned not null auto_increment,
+  tb_order varchar(50) not null comment '淘宝订单号',
   budget int not null default 0,
+  status tinyint(1) not null default 0 comment '0:init,1:auto(系统计算完毕)',
+  buyer int unsigned not null default 0 comment '购买人id',
+  buyer_plan tinyint(1) not null default 0,
+  buyer_earning int not null default 0 comment 'buyer_plan == 0 ? 0 or floor(budget*0.5)',
+  father int unsigned not null default 0 comment '一级邀请人id',
+  father_plan tinyint(1) not null default 0,
+  father_earning int not null default 0 comment '0 or floor(budget*0.1)',
+  grandpa int unsigned not null default 0 comment '二级邀请人id',
+  grandpa_plan tinyint(1) not null default 0,
+  grandpa_earning int not null default 0 comment '0 or floor(budget*0.1)',
+  chain_a_earner int unsigned not null default 0 comment 'nearest partner or super',
+  chain_a_plan tinyint(1) not null default 0,
+  chain_a_earning int not null default 0 comment 'partner:floor(budget*0.15);super:floor(budget*0.2)',
+  chain_b_earner int unsigned not null default 0 comment 'secondary nearest partner or super only when chain_a_plan is partner',
+  chain_b_plan tinyint(1) not null default 0,
+  chain_b_earning int not null default 0 comment 'floor(budget*0.05)',
+  super_earner int not null default 0,
+  super_earning int not null default 0 comment 'super_earner!=0:floor(budget*0.05);others:0',
+  margin int not null default 0 comment 'budget-buyer_earning-father_earning-grandpa_earning-chain_a_earning-chain_b_earning-super_earning',
   primary key (id)
 ) comment '所有金额单位为人民币分,1表示0.01元,100表示1元';
 
 ################################################################################
-# 每加入一个付费会员触发：
-#     plan从普通会员升级为付费会员
-#     检查邀请者是否升级为梦想合伙人
-#     如果邀请者升级为梦想合伙人，检查邀请者的邀请者是否升级为超级合伙人
-# 每产生一笔佣金触发：
-#     1.顾客为普通会员
-#     2.顾客为付费会员
-#     3.顾客为梦想合伙人
-#     4.顾客为超级合伙人
+# 佣金展示（商品展示）
+################################################################################
+# 普通会员无佣金
+# 其他会员，显示自购返佣（总佣金的50%）
+################################################################################
+
+################################################################################
+# 佣金分配
+################################################################################
+# 50% 自购佣金
+# 10% 分销佣金
+#   追溯两级，共20%
+# 20% 奖金池
+#   仅梦想合伙人和超级合伙人
+#     A->B->C-D->E->F(梦想)->G->H->I(超级)->J->K: f 15%, I 5%
+#     A->B->C-D->E->F(梦想)->G->H->I(梦想)->J->K: f 15%, I 5%
+#     A->B->C-D->E->F(超级)->G->H->I(梦想)->J->K: f 20%, I 0
+# 5% 平级奖
+#   仅一个超级合伙人A，发展出了另外一个超级合伙人B，并且B没有发展任何超级合伙人，则A获取5%的平级奖
+# 5% 结余
 ################################################################################
