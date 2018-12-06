416bae02af35cfb0401a09df923b7a72cd78a2fc
jibo <jibo@outlook.com>
2018-12-06 17:53:06 +0800

    Script to merge product data

---
+++ scripts/2018_12_06_merge_product.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: jibo
+ * Date: 2018-12-06
+ * Time: 17:23
+ */
+
+use db\SqlMapper;
+
+require_once __DIR__ . '/index.php';
+
+function logging($log)
+{
+    if (is_scalar($log)) {
+        echo $log, PHP_EOL;
+        $logger = new Log(date('Y-m-d.\l\o\g'));
+        $logger->write($log);
+    } else {
+        print_r($log);
+    }
+}
+
+
+$old = new SqlMapper('product_raw');
+$new = new SqlMapper('product');
+
+$old->load();
+
+while (!$old->dry()) {
+    $data =[
+        'affiliate' => $old['affiliate'],
+        'hc' => $old['hc'],
+        'tid' => $old['tid'],
+        'title' => $old['title'],
+        'thumb' => $old['thumb'],
+        'url' => $old['couponShortUrl'],
+        'code' => $old['couponCode'],
+        'price' => $old['price'],
+        'coupon' => $old['couponValue'],
+        'expire_date' => date('Y-m-d', strtotime($old['couponEnd'])),
+        'commission_rate' => $old['commissionRate'],
+        'commission_value' => $old['commissionValue'],
+    ];
+    $old->next();
+    $new->reset();
+    $new->copyfrom($data);
+    $new->save();
+}
---
+++ scripts/index.php
@@ -0,0 +1,12 @@
+<?php
+
+define('ROOT', dirname(__DIR__));
+
+require_once ROOT. '/vendor/autoload.php';
+
+call_user_func(function (Base $f3) {
+    $f3->mset([
+        'AUTOLOAD' => ROOT . '/src/',
+        'LOGS' => ROOT . '/runtime/logs/',
+    ]);    $f3->config(ROOT . '/cfg/system.ini,' . ROOT . '/cfg/debug.ini');
+}, Base::instance());
